233fd8d43e81dac0a8f65b7b39073d1b101409dd
Giovanni Juliao <gjuliao32@gmail.com>
2023-02-21 17:09:56 -0500

    Added queries

--- queries.sql
+++ queries.sql
@@ -9,3 +9,41 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT name FROM animals WHERE name NOT IN ('Gabumon');
 SELECT * FROM animals WHERE weight_kg >= 10.6 AND weight_kg <= 17.3;
+
+/*Part 2 */
+/* 1 */
+ALTER TABLE animals ADD COLUMN species VARCHAR ( 100 );
+
+/* 2 */
+BEGIN;
+ALTER TABLE animals RENAME COLUMN species TO unspecified;
+ROLLBACK;
+
+/* 3 */
+BEGIN;
+UPDATE animals SET species = 'Digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = COALESCE(species, 'Pokemon');
+COMMIT;
+
+/* 4 */
+BEGIN;
+DELETE FROM ANIMALS;
+ROLLBACK;
+
+/* 5 */
+BEGIN;
+SAVEPOINT original;
+DELETE FROM animals WHERE date_of_birth > ‘2022-01-01;
+SAVEPOINT o1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SAVEPOINT o1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+/* 6 */
+SELECT COUNT (*) FROM animals;
+SELECT COUNT (*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT name, escape_attempts FROM animals WHERE escape_attempts = ( SELECT MAX(escape_attempts) FROM animals);
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
