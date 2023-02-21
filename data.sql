4cb52a82e591f181f9d4b421b6ef8b0d00f0d265
Giovanni Juliao <gjuliao32@gmail.com>
2023-02-21 09:53:49 -0500

    Added species column

--- data.sql
+++ data.sql
@@ -5,3 +5,7 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (3, 'Pikachu', '2021-01-07', 1, false, 15.04);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (3, 'Devimon', '2017-05-12', 5, true, 11);
 
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (5, 'Charmander', '2020-02-08', 0, false, 11);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (6, 'Charmander', '2020-02-08', 0, false, 11);
+
+
