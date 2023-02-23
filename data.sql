956c1ea384e386e578aaa5321bcf7211d88dd20f
Giovanni Juliao <gjuliao32@gmail.com>
2023-02-23 07:35:06 -0500

    Fixed relation between tables

--- data.sql
+++ data.sql
@@ -24,12 +24,11 @@ INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
 INSERT INTO species (name) VALUES ('Pokemon');
 INSERT INTO species (name) VALUES ('Digimon');
 
-UPDATE animals SET owner_id = 'Dean Winchester' WHERE name = 'Boarmon';
-UPDATE animals SET owner_id = 'Dean Winchester' WHERE name = 'Angemon';
-UPDATE animals SET owner_id = 'Melody Pond' WHERE name = 'Blossom';
-UPDATE animals SET owner_id = 'Melody Pond' WHERE name = 'Squirtle';
-UPDATE animals SET owner_id = 'Melody Pond' WHERE name = 'Charmander';
-UPDATE animals SET owner_id = 'Bob' WHERE name = 'Plantmon';
-UPDATE animals SET owner_id = 'Bob' WHERE name = 'Devimon';
-UPDATE animals SET owner_id = 'Jennifer Orwell' WHERE name = 'Pikachu';
-UPDATE animals SET owner_id = 'Jennifer Orwell' WHERE name = 'Gabumon';
+/* Modify animals to so it includes species_id value */
+UPDATE animals SET species_id = 2 WHERE name LIKE '%mon';
+/* Modify your inserted animals to include owner information */
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
